6fb434b78fdbf407c7a94213867778b6ede932d0
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2015-12-11 01:03:04 -0200

    Bumped version to 0.1.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='dotcastles',
     packages=['dotcastles'],
-    version='0.1.2',
+    version='0.1.3',
     description='Shares your dotfiles through git',
     author='Ricardo Pescuma Domenecci',
     author_email='ricardo@pescuma.org',
     license='GPL',
     url='https://github.com/pescuma/dotcastles',
-    download_url='https://github.com/pescuma/dotcastles/tarball/v0.1.2',
+    download_url='https://github.com/pescuma/dotcastles/tarball/v0.1.3',
     keywords=['dotfiles'],
     classifiers=[],
     entry_points={
