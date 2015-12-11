331df95bcae73ec59955e2dff1dbb0f0102f1887
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2015-12-11 00:38:56 -0200

    Bumped version to 0.1.2

--- dotcastles/dotcastles.py
+++ dotcastles/dotcastles.py
@@ -25,7 +25,7 @@ def main():
     cmd.add_argument('castle', help='name of the castle')
     cmd.add_argument('file', type=argparse.FileType('r'), help='file name (must be inside home folder)')
 
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('--version', action='version', version='%(prog)s 0.1.2')
 
     if len(sys.argv) < 2:
         sys.argv.append('--help')
